506b5292d59ad53e12c5140afaa20dc316968dee
Duong Ba Nhat <banhatduong@gmailcom>
2023-09-09 20:13:11 +0700

    Revert "upload project"
    This reverts commit 41b15265d79999ba497e1bd9bd79959e7637990a.

--- 01-starting-setup/src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
-//đây là file đầu tiên được thực thi
