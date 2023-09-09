41b15265d79999ba497e1bd9bd79959e7637990a
Duong Ba Nhat <banhatduong@gmailcom>
2023-09-09 20:09:17 +0700

    upload project

---
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,16 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem expense={props.expenses[0]}></ExpenseItem>
+      <ExpenseItem expense={props.expenses[1]}></ExpenseItem>
+      <ExpenseItem expense={props.expenses[2]}></ExpenseItem>
+      <ExpenseItem expense={props.expenses[3]}></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
