7b942650f2648c7656528d67aeb6e3e7d002ccea
Duong Ba Nhat <banhatduong@gmailcom>
2023-09-13 16:50:38 +0700

    a complete practice project

--- 02-commplete-practice-project/src/App.js
+++ 02-commplete-practice-project/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
+
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const addUserHandler = (user) => {
+    setUsers((prevUsers) => {
+      return [...prevUsers, user];
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={users} />
     </div>
   );
 }
