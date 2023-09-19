a66fff526fc6afe816593c95c407573132d36b17
Duong Ba Nhat <banhatduong@gmailcom>
2023-09-19 22:08:48 +0700

    Configuring Custom Hooks

--- 07-building-custom-react-hooks/src/components/BackwardCounter.js
+++ 07-building-custom-react-hooks/src/components/BackwardCounter.js
@@ -1,9 +1,8 @@
-
 import useCounter from '../hooks/use-counter';
 import Card from './Card';
 
 const BackwardCounter = () => {
-  const counter = useCounter();
+  const counter = useCounter(false);
 
   return <Card>{counter}</Card>;
 };
