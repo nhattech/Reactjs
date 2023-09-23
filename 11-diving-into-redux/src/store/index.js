ee3df56a3e7c1806d99ab7983932d32de93a4501
Duong Ba Nhat <banhatduong@gmailcom>
2023-09-23 22:35:09 +0700

    Working with Multiple Slices

--- 11-diving-into-redux/src/store/index.js
+++ 11-diving-into-redux/src/store/index.js
@@ -22,11 +22,33 @@ const counterSlice = createSlice({
     },
   },
 });
+
+//Authentication
+const initialAuthState = {
+  isAuthentication: false,
+};
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthentication = true;
+    },
+    logout(state) {
+      state.isAuthentication = false;
+    },
+  },
+});
+
 //create store
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
