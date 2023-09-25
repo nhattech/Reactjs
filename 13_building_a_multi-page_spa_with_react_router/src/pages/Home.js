a49bed0741bfd58b0aabd5ef0a60bdc6fdda0127
Duong Ba Nhat <banhatduong@gmailcom>
2023-09-25 16:43:37 +0700

    Navigating between Pages with Links-a

--- 13_building_a_multi-page_spa_with_react_router/src/pages/Home.js
+++ 13_building_a_multi-page_spa_with_react_router/src/pages/Home.js
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>Go to <a href="/products">the products pages</a></p>
+    </>
+  );
 };
 
 export default Home;
