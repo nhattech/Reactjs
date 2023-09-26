252b6260ae530c46152a428e4dcd152ed0a24c14
Duong Ba Nhat <banhatduong@gmailcom>
2023-09-26 08:24:15 +0700

    Navigating Programmatically

--- 13_building_a_multi-page_spa_with_react_router/src/pages/Home.js
+++ 13_building_a_multi-page_spa_with_react_router/src/pages/Home.js
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate('/products');
+  }
   return (
     <>
       <h1>My Home Page</h1>
       <p>Go to <a href="/products">the products pages</a></p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </>
   );
 };
