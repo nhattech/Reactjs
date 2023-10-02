1dcc56842da8cfec73bdbe089886de8a2a8d3a31
Duong Ba Nhat <banhatduong@gmailcom>
2023-10-02 08:51:43 +0700

    Managing the Token Expiration

--- 15-Adding-authentication-to-react-apps/frontend/src/pages/Logout.js
+++ 15-Adding-authentication-to-react-apps/frontend/src/pages/Logout.js
@@ -1,7 +1,8 @@
 import { redirect } from 'react-router-dom';
-import { removeAuthToken } from '../util/auth';
+import { removeAuthToken, removeExpirationToken } from '../util/auth';
 
 export function action() {
   removeAuthToken();
+  removeExpirationToken();
   return redirect('/auth?mode=login');
 }
--- 15-Adding-authentication-to-react-apps/frontend/src/util/auth.js
+++ 15-Adding-authentication-to-react-apps/frontend/src/util/auth.js
@@ -4,14 +4,41 @@ export const setAuthToken = (token) => {
   localStorage.setItem('token', token);
 };
 
+export const setExpirationToken = () => {
+  const expirationDate = new Date();
+  expirationDate.setHours(expirationDate.getHours() + 1);
+  localStorage.setItem('expiration', expirationDate.toISOString());
+};
+
+export const getTokenDuration = () => {
+  const storageExpiration = localStorage.getItem('expiration');
+
+  const expirationDate = new Date(storageExpiration);
+  const now = new Date();
+
+  return expirationDate.getTime() - now.getTime();
+};
+
 export const getAuthToken = () => {
   const token = localStorage.getItem('token');
+
+  if(!token) {
+    return null;
+  }
+  const tokenDuration = getTokenDuration();
+  if (tokenDuration < 0) {
+    return 'EXPIRED';
+  }
 
   return token;
 };
 
 export const removeAuthToken = () => {
   localStorage.removeItem('token');
+};
+
+export const removeExpirationToken = () => {
+  localStorage.removeItem('expiration');
 };
 
 export const tokenLoader = () => {
